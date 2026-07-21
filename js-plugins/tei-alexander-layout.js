f4a39169cb7b4c7e9195bf01f2b7a0f40047ef6d
omarkhalaf81 <74829825+omarkhalaf81@users.noreply.github.com>
2026-07-21 08:40:40 +0200

    Refactor tei-alexander-layout for improved line-column structure

--- js-plugins/tei-alexander-layout.js
+++ js-plugins/tei-alexander-layout.js
@@ -70,6 +70,7 @@
     }
 
     console.log('lineMap costruita', Object.keys(lineMap).length);
+    window.__teiLineMap = lineMap;
   }
 
   function hasClass(node, className) {
@@ -169,128 +170,117 @@ function findRenderedLine(lbId, info) {
     return false;
   }
 
-  function applyColumns() {
-    if (running) {
-      return;
-    }
-
-    running = true;
-
-    var pages = {};
-    var ids = Object.keys(lineMap);
-
-    for (var i = 0; i < ids.length; i++) {
-      var id = ids[i];
-      var info = lineMap[id];
-      var line = findRenderedLine(id, info);
-
-      if (!line || insideColumns(line)) {
-        continue;
-      }
-
-      if (!pages[info.page]) {
-        pages[info.page] = [];
-      }
-
-      pages[info.page].push({
-        node: line,
-        column: info.column,
-        line: parseInt(info.line || '0', 10)
-      });
-    }
-
-    var pageNames = Object.keys(pages);
-
-    for (var p = 0; p < pageNames.length; p++) {
-      var page = pageNames[p];
-      var items = pages[page];
-
-      items.sort(function (a, b) {
-        return a.line - b.line;
-      });
-
-      if (!items.length) {
-        continue;
-      }
-
-      var first = items[0].node;
-
-      if (!first || !first.parentNode) {
-        continue;
-      }
-
-      var wrapper = document.createElement('div');
-      wrapper.className = 'tei-page-columns';
-      wrapper.setAttribute('data-tei-page', page);
-
-      var col1 = document.createElement('div');
-      col1.className = 'tei-column tei-column-1';
-      col1.setAttribute('data-tei-column', '1');
-
-      var col2 = document.createElement('div');
-      col2.className = 'tei-column tei-column-2';
-      col2.setAttribute('data-tei-column', '2');
-
-      wrapper.appendChild(col1);
-      wrapper.appendChild(col2);
-
-      var col2Anchor = document.createElement('span');
-        col2Anchor.className = 'tei-column-anchor';
-        col2Anchor.style.display = 'none';
-
-        col2.appendChild(col2Anchor);
-
-      first.parentNode.insertBefore(wrapper, first);
-
-      for (var j = 0; j < items.length; j++) {
-        var item = items[j];
-
-        if (!item.node || !item.node.parentNode) {
-          continue;
-        }
-
-        if (!hasClass(item.node, 'tei-line')) {
-          item.node.className += ' tei-line';
-        }
-
-        item.node.setAttribute('data-tei-column', item.column);
-        var lbInLine = item.node.querySelector('span.lb, lb, .lb');
-
-if (lbInLine && !lbInLine.querySelector('.lineN')) {
-  var visibleNumber =
-    item.line ||
-    lbInLine.getAttribute('n') ||
-    lbInLine.getAttribute('data-n') ||
-    '';
-
-  if (visibleNumber) {
-    var br = document.createElement('br');
-
-    var lineNumber = document.createElement('span');
-    lineNumber.className = 'lineN';
-    lineNumber.textContent = visibleNumber;
-
-    lbInLine.appendChild(br);
-    lbInLine.appendChild(lineNumber);
+ function applyColumns() {
+  if (running) {
+    return;
   }
+
+  running = true;
+
+  var pages = {};
+  var ids = Object.keys(lineMap);
+
+  for (var i = 0; i < ids.length; i++) {
+    var id = ids[i];
+    var info = lineMap[id];
+    var line = findRenderedLine(id, info);
+
+    if (!line) {
+      continue;
+    }
+
+    if (!pages[info.page]) {
+      pages[info.page] = [];
+    }
+
+    pages[info.page].push({
+      node: line,
+      column: info.column,
+      line: parseInt(info.line || '0', 10)
+    });
+  }
+
+  var pageNames = Object.keys(pages);
+
+  for (var p = 0; p < pageNames.length; p++) {
+    var page = pageNames[p];
+    var items = pages[page];
+
+    items.sort(function (a, b) {
+      return a.line - b.line;
+    });
+
+    if (!items.length) {
+      continue;
+    }
+
+    var first = items[0].node;
+
+    if (!first || !first.parentNode) {
+      continue;
+    }
+
+    var container = first.parentNode;
+
+    if (!hasClass(container, 'tei-column-layout')) {
+      container.className += ' tei-column-layout';
+    }
+
+    container.setAttribute('data-tei-page', page);
+
+    for (var j = 0; j < items.length; j++) {
+      var item = items[j];
+
+      if (!item.node) {
+        continue;
+      }
+
+      if (!hasClass(item.node, 'tei-line')) {
+        item.node.className += ' tei-line';
+      }
+
+      item.node.setAttribute('data-tei-column', item.column);
+
+      if (hasClass(item.node, 'tei-column-break')) {
+        item.node.className = item.node.className.replace(/\btei-column-break\b/g, '').trim();
+      }
+
+      if (String(item.column) === '2') {
+        if (!hasClass(item.node, 'tei-column-break')) {
+          item.node.className += ' tei-column-break';
+        }
+      }
+
+      var lbInLine = item.node.querySelector('span.lb, lb, .lb');
+
+      if (lbInLine && !lbInLine.querySelector('.lineN')) {
+        var visibleNumber =
+          item.line ||
+          lbInLine.getAttribute('n') ||
+          lbInLine.getAttribute('data-n') ||
+          '';
+
+        if (visibleNumber) {
+          var br = document.createElement('br');
+
+          var lineNumber = document.createElement('span');
+          lineNumber.className = 'lineN';
+          lineNumber.textContent = visibleNumber;
+
+          lbInLine.appendChild(br);
+          lbInLine.appendChild(lineNumber);
+        }
+      }
+    }
+  }
+
+  console.log(
+    'tei-column-layout create',
+    document.querySelectorAll('.tei-column-layout').length
+  );
+
+  running = false;
 }
-
-        if (String(item.column) === '2') {
-          col2.appendChild(item.node);
-        } else {
-          col1.appendChild(item.node);
-        }
-      }
-    }
-
-    console.log(
-      'tei-page-columns create',
-      document.querySelectorAll('.tei-page-columns').length
-    );
-
-    running = false;
-  }
-
   function start() {
     loadText('config/config.json', function (configText) {
       var config = JSON.parse(configText);
